0e3b99f3044ce013a773eeedfba04b720b851d74
kelvin-keegen <kelvinkeegen17@gmail.com>
2021-10-21 17:02:45 +0300

    Added Spring cloud config server

--- microservice3/microservice3/src/main/java/com/keegan/demo/microservice3/controller/CarController.java
+++ microservice3/microservice3/src/main/java/com/keegan/demo/microservice3/controller/CarController.java
@@ -5,10 +5,8 @@ import com.keegan.demo.microservice3.entity.Car;
 import com.keegan.demo.microservice3.entity.CarRequestBody;
 import com.keegan.demo.microservice3.service.CarPublishService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -16,11 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CarController {
 
     private final CarPublishService carPublishService;
+    private final Environment environment;
 
     @PostMapping(path = "/car")
     public Car RequestCar(@RequestBody CarRequestBody requestBody) {
 
         return carPublishService.ConsumeData(requestBody);
+    }
+
+    @GetMapping(path = "/env")
+    public String CheckEnvironment() {
+        return environment.toString();
     }
 
 
